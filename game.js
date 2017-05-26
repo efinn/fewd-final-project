742b0f135542f043d3d4cbe50d5d9e57a792c9b4
efinn <ellen@Ellens-MacBook-Pro.local>
2017-05-25 21:09:56 -0400

    aesthetic changes

--- game.js
+++ game.js
@@ -1,37 +1,33 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-game', { preload: preload, create: create, update: update });
 
 function preload() {
     game.load.image('sky', 'assets/sky.png');
-    game.load.image('ground', 'assets/platform.png');
+    game.load.image('ground', 'assets/platform2.png');
     game.load.image('treat', 'assets/treat.png');
     game.load.spritesheet('ziggy', 'assets/ziggysprite2.png', 32, 48);
     game.load.image('enemy', 'assets/skate2.png')
     //  Firefox doesn't support mp3, use ogg
     game.load.audio('interstellar', ['assets/interstellar.mp3', 'assets/interstellar.ogg']);
-
     //game frame will scale to page size
     this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     this.scale.pageAlignHorizontally = true;
     this.scale.pageAlignVertically = true;
     this.scale.setScreenSize(true); 
-}
+    }
 
-var player;
 var platforms;
 var cursors;
-
-var treats;
 var score = 0;
 var scoreText;
-
+var player;
 var enemy;
+var treats;
 
 function create() {
-
     // Add music
     music = game.add.audio('interstellar');
     music.play();
-
+    
     //  Enable physics - Arcade Physics
     game.physics.startSystem(Phaser.Physics.ARCADE);
     
@@ -46,59 +42,52 @@ function create() {
     var ground = platforms.create(0, game.world.height - 64, 'ground');
     ground.scale.setTo(2, 2);
     ground.body.immovable = true;
-    
     var ledge = platforms.create(400, 400, 'ground');
     ledge.body.immovable = true;
-
     ledge = platforms.create(-150, 250, 'ground');
     ledge.body.immovable = true;
-
+    
     // Define player & enable physics
     player = game.add.sprite(32, game.world.height - 150, 'ziggy');
     game.physics.arcade.enable(player);
     player.body.bounce.y = 0.1;
     player.body.gravity.y = 300;
     player.body.collideWorldBounds = true;
-
     player.animations.add('left', [0, 1, 2, 3], 10, true);
     player.animations.add('right', [5, 6, 7, 8], 10, true);
-
+    
     // Add treats
     treats = game.add.group();
     treats.enableBody = true;
-
+        
     for (var i = 0; i < 5; i++) {
-        var treat = treats.create(i * 70, 0, 'treat');
-        treat.body.gravity.y = 300;
-        treat.body.bounce.y = 0.3 + Math.random() * 0.2;
-        }
+            var treat = treats.create(i * 70, 0, 'treat');
+            treat.body.gravity.y = 300;
+            treat.body.bounce.y = 0.3 + Math.random() * 0.2;
+    }
 
     // Add enemies & enable physics
     enemies = game.add.group();
     enemies.enableBody = true;
     game.physics.arcade.enable(enemies);
-
   
     for (var i = 0; i < 2; i++) {
         var enemy = enemies.create(game.world.randomX, game.world.randomY, 'enemy');
         enemy.body.gravity.y = 550;
         enemy.body.bounce.y = 0.3 + Math.random() * 0.2;
-        enemy.body.velocity.setTo(10 + Math.random() * 40, 10 + Math.random() * 40);
+        //enemy.body.velocity.setTo(10 + Math.random() * 40, 10 + Math.random() * 40);
         enemy.body.collideWorldBounds = true;
-        }
-
+        enemy.body.velocity.x = 80;
+    }
 
     // Add score
     scoreText = game.add.text(15, 15, 'score: 0', {fontSize: '32px', fill: '#000'});
     
     // Add controls
     cursors = game.input.keyboard.createCursorKeys();
-
 }
 
-
 function update() {
-
     // Enable collision between player and platforms
     var hitPlatform = game.physics.arcade.collide(player, platforms);
     game.physics.arcade.collide(treats, platforms);
@@ -106,26 +95,21 @@ function update() {
     game.physics.arcade.overlap(player, treats, getTreat, null, this);
     game.physics.arcade.overlap(player, enemies, killEnemy, null, this);
 
-    // Define movement
     player.body.velocity.x = 0;
-    if (cursors.left.isDown)
-        {
-            player.body.velocity.x = - 150;
-            player.animations.play('left');
-        }
-    else if (cursors.right.isDown)
-        { 
-            player.body.velocity.x = 150;
-            player.animations.play('right');
-        }
-    else
-        {
-            player.animations.stop();
-            player.frame = 4;
-        }
+    if (cursors.left.isDown) {
+        player.body.velocity.x = - 150;
+        player.animations.play('left');
+    }
+    else if (cursors.right.isDown) { 
+        player.body.velocity.x = 150;
+        player.animations.play('right');
+    }
+    else {
+        player.animations.stop();
+        player.frame = 4;
+    }
 
-    if (cursors.up.isDown && player.body.touching.down && hitPlatform)
-    {
+    if (cursors.up.isDown && player.body.touching.down && hitPlatform) {
         player.body.velocity.y = -350;
     }
 
@@ -138,17 +122,18 @@ function update() {
         scoreText.text = 'Score: ' + score;
     }
 
+    //enemy removed from screen
     function killEnemy (player, enemy) {
-        //enemy removed from screen
-         if ( enemy.body.touching.up )    {          
+        if ( enemy.body.touching.up ) {          
             enemy.kill();   
-
-       }   
-        else     {      
-             player.kill();    
-
+        }   
+        else {      
+            player.kill();   
+            alert('Don\'t let the skateboards hit you! Try again.');
+            location.reload(); 
+        }
         //update score
         score += 10;
         scoreText.text = 'Score: ' + score;
-}}}
-
+    }
+}
