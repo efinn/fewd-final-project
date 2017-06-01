9c3aaedc993f50690bf660ed3eae73116510a006
efinn <ellen@Ellens-MacBook-Pro.local>
2017-06-01 00:27:22 -0400

    randomized treats
    Looped music, changed background, randomized treat spawn points

--- game.js
+++ game.js
@@ -1,11 +1,11 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-game', { preload: preload, create: create, update: update });
 
 function preload() {
-    game.load.image('sky', 'assets/sky.png');
+    game.load.image('home', 'assets/home2.png');
     game.load.image('ground', 'assets/platform2.png');
     game.load.image('treat', 'assets/treat.png');
     game.load.spritesheet('ziggy', 'assets/ziggysprite2.png', 32, 48);
-    game.load.spritesheet('enemy', 'assets/mouse.png')
+    game.load.spritesheet('enemy', 'assets/mouse.png', 32, 48);
     //  Firefox doesn't support mp3, use ogg
     game.load.audio('interstellar', ['assets/interstellar.mp3', 'assets/interstellar.ogg']);
     //game frame will scale to page size
@@ -27,13 +27,14 @@ function create() {
     // Add music
     music = game.add.audio('interstellar');
     music.play();
+    music.loop = true;
     
     //  Enable physics - Arcade Physics
     game.physics.startSystem(Phaser.Physics.ARCADE);
     
     //  Add background
-    game.add.sprite(0, 0, 'sky');
-    
+    game.add.sprite(0, 0, 'home');
+
     //  Add platforms (ground and ledges) and enable physics for the group
     platforms = game.add.group();
     platforms.enableBody = true;
@@ -55,16 +56,6 @@ function create() {
     player.body.collideWorldBounds = true;
     player.animations.add('left', [0, 1, 2, 3], 10, true);
     player.animations.add('right', [5, 6, 7, 8], 10, true);
-    
-    // Add treats for Ziggy to gather
-    treats = game.add.group();
-    treats.enableBody = true;
-        
-    for (var i = 0; i < 5; i++) {
-            var treat = treats.create(i * 70, 0, 'treat');
-            treat.body.gravity.y = 300;
-            treat.body.bounce.y = 0.3 + Math.random() * 0.2;
-    }
 
     // Add enemies & enable physics for them
     enemies = game.add.group();
@@ -75,9 +66,18 @@ function create() {
         var enemy = enemies.create(game.world.randomX, game.world.randomY, 'enemy');
         enemy.body.gravity.y = 550;
         enemy.body.bounce.y = 0.3 + Math.random() * 0.2;
-        //enemy.body.velocity.setTo(10 + Math.random() * 40, 10 + Math.random() * 40);
         enemy.body.collideWorldBounds = true;
-        enemy.body.velocity.x = 80;
+        enemy.body.velocity.x = 90;
+    }
+
+    // Add treats for Ziggy to gather
+    treats = game.add.group();
+    treats.enableBody = true;
+        
+    for (var i = 0; i < 5; i++) {
+            var treat = treats.create(game.world.randomX, game.world.randomY, 'treat');
+            treat.body.gravity.y = 300;
+            treat.body.bounce.y = 0.3 + Math.random() * 0.2;
     }
 
     // Add score
